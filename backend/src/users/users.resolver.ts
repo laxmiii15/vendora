71a455366b7cd579367f208e3bf7e69e12dfb119
laxmiii15 <khanallaxmii115@gmail.com>
2026-06-05 22:15:33 +0545

    feat:implemented users module

---
+++ backend/src/users/users.resolver.ts
@@ -0,0 +1,18 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => [User], { name: 'users' })
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Query(() => User, { name: 'user' })
+  findById(@Args('id') id: string) {
+    return this.usersService.findById(id);
+  }
+}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  }
+
+  async findById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
